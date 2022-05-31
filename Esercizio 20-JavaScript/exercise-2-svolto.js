f8a60480a2d8d0d4acfd2aac0e18c119b13d2d9e
JennyPersichini <101724640+JennyPersichini@users.noreply.github.com>
2022-05-31 10:44:39 +0200

    Esercizio 20 - Object copying - JS

---
+++ Esercizio 20-JavaScript/exercise-2-svolto.js
@@ -0,0 +1,16 @@
+const person1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25
+  };
+  
+  const person2 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25
+  };
+  
+  person2.firstName = 'Simon';
+  
+  console.log(person1);
+  console.log(person2);
