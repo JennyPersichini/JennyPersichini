62d6e59c18e3a0901bee5d55b42bb2a1d32965bc
JennyPersichini <101724640+JennyPersichini@users.noreply.github.com>
2022-06-05 15:00:57 +0200

    corretto

---
+++ Esercizio 19-JavaScript/exercise-1-svolto.js
@@ -0,0 +1,14 @@
+const person1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25
+};
+
+const person2 = person1;
+
+person2.firstName = 'Simon';
+
+console.log(person1);
+console.log(person2);
+
+// Modificando l'oggetto person2 viene modificato anche l'oggetto person1 perchè const person2 = person1
