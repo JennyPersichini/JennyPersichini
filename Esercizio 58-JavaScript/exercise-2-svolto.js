c1117353effdb15380b54e74a60f0b5a50d4e98b
JennyPersichini <101724640+JennyPersichini@users.noreply.github.com>
2022-06-22 14:47:12 +0200

    Esercizio 58 - Async Management  - JS

---
+++ Esercizio 58-JavaScript/exercise-2-svolto.js
@@ -0,0 +1,34 @@
+const persons = [
+    {
+      id: 1,
+      firstName: 'Mario',
+      lastName: 'Rossi',
+      age: 25
+    },
+    {
+      id: 2,
+      firstName: 'Maria',
+      lastName: 'Verdi',
+      age: 32
+    },
+    {
+      id: 3,
+      firstName: 'Giovanni',
+      lastName: 'Rossi',
+      age: 35
+    }
+  ];
+  
+  function fetchPersonById(id) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if(persons.find(item => item.id === id)) {
+        resolve(person);
+      } else 
+        reject('non esiste una persona con questo id');
+      },
+        1000); 
+    });
+  }
+  
+  fetchPersonById(4).then((person) => console.log(person));
