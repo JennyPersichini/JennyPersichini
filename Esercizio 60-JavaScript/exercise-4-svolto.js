d04bf8c9f3f2b6b646c8f0a039b074f559aab903
JennyPersichini <101724640+JennyPersichini@users.noreply.github.com>
2022-06-28 14:17:12 +0200

    Esercizio 60 - Async Management - JS

---
+++ Esercizio 60-JavaScript/exercise-4-svolto.js
@@ -0,0 +1,63 @@
+const persons = [
+  {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  },
+  {
+    id: 2,
+    firstName: 'Maria',
+    lastName: 'Verdi',
+    age: 32
+  },
+  {
+    id: 3,
+    firstName: 'Giovanni',
+    lastName: 'Rossi',
+    age: 35
+  }
+];
+
+const jobs = [
+  {
+    id: 1,
+    jobTitle: 'CEO'
+  },
+  {
+    id: 2,
+    jobTitle: 'Project Manager'
+  },
+  {
+    id: 3,
+    jobTitle: 'Developer'
+  }
+];
+
+function fetchPersonById(id) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const person = persons.find(person => person.id === id);
+      if (person) {
+        resolve(person);
+      }
+    }, 1000);
+  });
+}
+
+function fetchJobById(id) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const job = jobs.find(job => job.id === id);
+      if (job) {
+        resolve(job);
+      }
+    }, 1000);
+  });
+}
+
+Promise.all([fetchPersonById(1), fetchJobById(1)])
+  .then((person) => console.log(person))
+
+
+
